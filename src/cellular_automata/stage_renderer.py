49f420170e2a2ee8e783585d7bf83b8b883d5a71
JJWRoeloffs <jelleroeloffs@gmail.com>
2022-11-19 11:28:54 +0100

    Add pretty automata printer

---
+++ src/cellular_automata/stage_renderer.py
@@ -0,0 +1,43 @@
+import os
+
+import numpy as np
+from nptyping import NDArray
+from beartype import beartype
+
+
+class StageRenderer:
+    characters = [
+        " ",
+        "█",
+        "▒",
+        "░",
+        "▓",
+    ]
+
+    @beartype
+    @classmethod
+    def render_item(cls, item: np.int8) -> str:
+        """Returns a shaded unicode full block for an item"""
+        if item > 4:
+            raise NotImplementedError
+
+        # The numbers are nominals
+        # mapping to increasing opaqueness is not needd.
+        return cls.characters[item]
+
+    @beartype
+    @classmethod
+    def render(cls, stage: NDArray):
+        """render the given stage with this rule"""
+        print("".join(map(cls.render_item, stage)))
+
+    @beartype
+    @staticmethod
+    def get_simple_init() -> NDArray:
+        """Returns an array of the width of the screen that's all 0s with one 1 in the middle.
+        Usefull to get a starting point when printing
+        """
+        screen_width = int(os.popen("stty size", "r").read().split()[1])
+        init = np.zeros(screen_width, dtype=np.int8)
+        init[int(screen_width / 2)] = 1
+        return init
---
+++ src/cellular_automata/terminal_automata.py
@@ -0,0 +1,50 @@
+import time
+import argparse
+
+from .automata import RuleSet
+from .stage_renderer import StageRenderer
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="""Print increasing stages of a cellular automata to the terminal
+        Starting with a 1 in the center, printing ten new stages a second.
+        """
+    )
+    parser.add_argument(
+        "-w",
+        "--wolfraam",
+        nargs="?",
+        default=30,
+        type=int,
+        help="The Wolfraam rule to use",
+    )
+    parser.add_argument(
+        "-k",
+        "--k",
+        nargs="?",
+        default=2,
+        type=int,
+        help="The amount of dimensions to use",
+    )
+    parser.add_argument(
+        "-r",
+        "--radius",
+        nargs="?",
+        default=1,
+        type=int,
+        help="The radius to use",
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = argparse.Namespace = parse_arguments()
+
+    stage = StageRenderer.get_simple_init()
+    rule_set = RuleSet(args.wolfraam, args.k, args.radius)
+
+    while True:
+        StageRenderer.render(stage)
+        stage = rule_set(stage)
+        time.sleep(0.1)
